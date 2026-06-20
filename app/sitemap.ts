5295b1896be0724332d0cc4bb491fe5d055ef9b4
Aleksandar Gojković <agojkovic92@gmail.com>
2026-06-20 16:14:45 +0200

    Fix sitemap URLs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,34 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://natural-stupidity.vercel.app",
+      url: "https://www.naturalstupidityapp.com",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
+    },
+    {
+      url: "https://www.naturalstupidityapp.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.naturalstupidityapp.com/privacy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.naturalstupidityapp.com/terms",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.naturalstupidityapp.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
     },
   ];
 }
